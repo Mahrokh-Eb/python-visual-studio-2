d54c9e64a3bc1eb4d376436a5efc6e67ae570ad8
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-09-12 21:15:16 -0700

    list intro

---
+++ 7-list.py
@@ -0,0 +1,11 @@
+myColor = ['red', 'pink', 'blue', 'green', 'gray', 'broWn' 'black', 'orange']
+
+for color in myColor:
+    print(f'the color is: {color}')
+
+print('==============================================')
+
+index = 0
+while index < (len(myColor)):
+    print(f'the color is: {myColor[index]}')
+    index += 1
