e4ffd68e940459adbffc46482b0b5d2641e2a044
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-09-13 17:08:50 -0700

    list Comprehension

--- 10-list4.py
+++ 10-list4.py
@@ -1,5 +1,5 @@
 # list Comprehension 
-myNumbers = [1,2,3,4,5]
+myNumbers = [1,2,3,4,5,6,7,8,9]
 
 doubledNumbers = []
 for i in myNumbers:
@@ -8,3 +8,12 @@ print(f'using for: {doubledNumbers}')
 
 doubledNumbers2 = [i*3 for i in myNumbers]
 print(f'using for list: {doubledNumbers2}')
+
+myName = 'Mahrokh'
+nameChar = [i.upper() for i in myName]
+print(nameChar)
+
+print('=================================================')
+
+newForTry =[i*2 if i%2==0 else i*3 for i in myNumbers]
+print(newForTry)
