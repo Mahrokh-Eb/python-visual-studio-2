49d9affe7f3464666945e0b701d0eb4edb8e4eaa
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-09-15 13:06:41 -0700

    defining the functions

--- 14-functions.py
+++ 14-functions.py
@@ -7,3 +7,16 @@ def sum(a,b):
 a = int(input('Enter first num! '))
 b = int(input('Enter second num! '))
 print(sum(a, b))
+
+def showFullNme(a,b):
+    return a+"  "+b
+a = 'Mahrokh'
+b = 'Ebrahimi'
+print(showFullNme(a,b))
+
+print('====================================================================')
+dictionary = { 'first_item': 'ghomeSabzi', 'second_item':'shambelile' }
+def sabziFunc(a, b):
+    return a + '  '+ b
+x = sabziFunc(dictionary['first_item'], dictionary['second_item'])
+print(x)
