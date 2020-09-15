e3e106b0187d973e92c1ec2bde45b0d1c78bb5df
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-09-15 12:19:20 -0700

    defining functions

---
+++ 14-functions.py
@@ -0,0 +1,9 @@
+def print_squre_of_7(): 
+    print(7*7)
+print_squre_of_7()
+
+def sum(a,b): 
+    return a+b
+a = int(input('Enter first num! '))
+b = int(input('Enter second num! '))
+print(sum(a, b))
