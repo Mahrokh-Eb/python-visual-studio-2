02f2e4a8afa2a1efd9527f445a57d2acc6fdf9f4
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-09-16 15:47:07 -0700

    functions 2

---
+++ 15-function2.py
@@ -0,0 +1,21 @@
+# making parameter of the function Dinamically 
+def parameterDynamic(name, *arg):
+    print(name)
+    total = 0
+    for i in arg:
+        total += i
+    return total
+print(parameterDynamic('sum of the numbers: ',1, 1, 1, 1, 1, 1)) # sum of the numbers: 6
+
+print('====================================================================================')
+#  printing like a dictionary
+def dictionary(**kwargs):
+    for i, j in kwargs.items():
+        print(i,':',j)
+dictionary(name='Mahi', lastname='Ebrahimi', DOB='September', age= 33, email='mebrahimi@ucla.edu')
+
+print('====================================================================================')
+# the order of the paRameters fo the function
+def orderdef(a, b, *args, c = 'defaullf', **kwargs ):
+    return[a, b, args, kwargs]
+print(orderdef(1, 2, 3, 4, m='fari' ))
