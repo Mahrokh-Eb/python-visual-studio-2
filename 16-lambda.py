70554cb056384da494c9113b3942291299320ba6
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-09-17 11:34:03 -0700

    lambda map()

--- 16-lambda.py
+++ 16-lambda.py
@@ -1,11 +1,25 @@
-name = ['mah', 'fatai', 'goldane', 'shakazm']
 
-upperNmaes = map(lambda name: name.upper(), name)
-print(list(upperNmaes))
-
-#map()
-numbers = [1,2,3,4,5,6] 
+#doubling numbers, using for 
+numbers = [1,2,3,4,5,6] # output: [2, 4, 6, 8, 10, 12]
 doubles =[]
 for i in numbers:
     doubles.append(i*2)
 print(doubles)
+
+# using map()
+double2 = map(lambda i : i*2, numbers)
+print(list(double2))
+
+# using map() to show char as uppercase
+name = ['mah', 'fatai', 'goldane', 'shakazm']
+uppernames = map(lambda name: name.upper(), name)
+print(name, list(uppernames))
+
+# make a dictionary in a list and return last name
+nameFamily = [{'first name ': 'Mona', 'last name': 'goldani'}, 
+{'first name ': 'lona', 'last name': 'laghali'}, 
+{'first name ': 'moryan', 'last name': 'moyalnizadeh'}]
+families = map(lambda i: i ['last name'].upper(), nameFamily)
+print(list(families))
+
+
