b0847b409836bfdee1cc5b80eeb33a8aa9ab7b1c
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-09-17 11:03:16 -0700

    lambda map

--- 16-lambda.py
+++ 16-lambda.py
@@ -2,3 +2,10 @@ name = ['mah', 'fatai', 'goldane', 'shakazm']
 
 upperNmaes = map(lambda name: name.upper(), name)
 print(list(upperNmaes))
+
+#map()
+numbers = [1,2,3,4,5,6] 
+doubles =[]
+for i in numbers:
+    doubles.append(i*2)
+print(doubles)
