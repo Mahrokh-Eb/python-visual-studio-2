715d6278c4848cf7b55484ca6e780a929ea0054b
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-09-18 16:39:24 -0700

    sort(), list, dictionary

--- 20-sorted.py
+++ 20-sorted.py
@@ -1,12 +1,13 @@
 # sorting items
 numbers = [1, 9, 3, 4, 7, 2, 8]
 numbers.sort()
-print(numbers)
+print(numbers) #[1, 2, 3, 4, 7, 8, 9]
 
 result2 = sorted(numbers)
-print(result2)
+print(result2) 
 print(numbers) # it shows previous number list again
 
 # sorting the dictionary
 users = [{'name': 'kobra'}, {'name': 'fati'}, {'name': 'gholi'}, {'name': 'golmamad'}, {'name': 'ozra'}]
 print(sorted(users, key= lambda i : i['name'])) 
+# output: [{'name': 'fati'}, {'name': 'gholi'}, {'name': 'golmamad'}, {'name': 'kobra'}, {'name': 'ozra'}]
