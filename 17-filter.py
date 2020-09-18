384b24dcd6622842a5d065fa3021646ee856023d
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-09-17 18:03:39 -0700

    lambda, filter(), map()

--- 17-filter.py
+++ 17-filter.py
@@ -6,19 +6,19 @@ users = [{'name': 'fati', 'Shopping card':[]},
 {'name': 'safoora', 'Shopping card':['kotlin', 'python']},
 {'name': 'sogholme', 'Shopping card':[]}
 ]
-print(len(users))
-result = filter(lambda i: len(i['Shopping card'])==0, users)
-print(list(result))
+print(len(users))  # 3
+result = filter(lambda i: len(i['Shopping card'])==0, users)  
+print(list(result)) # [{'name': 'fati', 'Shopping card': []}, {'name': 'sogholme', 'Shopping card': []}]
 
-# combining filter() & map
 # first give all user's name
 result1 = map(lambda i: i['name'], users)  
-print(list(result1))    
+print(list(result1)) # ['fati', 'safoora', 'sogholme']
 
+# combining filter() & map
 # it gives the users who their Shopping card is empty 
 result2 = map(lambda i: i['name'], filter(lambda i: len(i['Shopping card'])==0, users))  
-print(list(result2))  
+print(list(result2))  # ['fati', 'sogholme']
 
-# it gives the users who their Shopping card is empty with for
+# it gives the users who their Shopping card is empty with for instead of map() & filter()
 result3 = [i['name'] for i in users if len(i['Shopping card'])==0]
-print(result3)
+print(result3) #['fati', 'sogholme']
