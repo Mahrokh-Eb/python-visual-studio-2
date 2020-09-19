386d31e9c31dcd9745fd15576d9c4c020982761f
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-09-19 15:36:56 -0700

    max(), min()

---
+++ 21-max-min.py
@@ -0,0 +1,9 @@
+# built in Functions 
+numbers = [2, 6, 7, 18, 90]
+print('the maximum of above list is: ',max(numbers)) #  90
+
+names = ['sara', 'mamad', 'golbahar', 'sajadSogholme', 'sharareh', 'zolfaghar']
+print(max(names))
+print(min(names))
+print(len(names))
+print(max(len(i)) for i in names)
--- 21-max.py
+++ 21-max.py
@@ -1,6 +1,14 @@
+# built in Functions ,j44
 numbers = [2, 6, 7, 18, 90]
-print(max(numbers))
+print('the maximum of above list is: ',max(numbers)) #  90
 
-names = ['sara', 'mamad', 'golbahar', 'sajadSogholme', 'sharareh']
-print(len(names))
-print(max(len(i)) for i in names)
+names = ['sara', 'mamad', 'golbahar', 'sajadSogholme', 'sharareh', 'zolfaghar']
+print(max(names)) #zolfaghar
+print(min(names)) #golbahar
+ 
+print('len(names)= ',len(names)) #how many items does the names list have , 6
+result = [len(i) for i in names] #it gives the size of char in per name = [4, 5, 8, 13, 8, 9]
+print('size of char in per name = ',result)
+
+print(max(names, key= lambda i: len(i))) # the name that has more lenght = sajadSogholme
+
