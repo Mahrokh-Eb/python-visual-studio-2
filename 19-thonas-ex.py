5782d8f81125fc7d1f835a7717424e5162ba4355
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-09-18 21:39:05 -0700

    practice tweet

--- 19-thonas-ex.py
+++ 19-thonas-ex.py
@@ -2,24 +2,27 @@ import re
 
 # opening sample_tweets file
 f = open("sample_tweets 2.txt", "r")
+# print(f.read())
 
 # asking user to enter wanted word
-txt = input('please enter a word! like Ariana: ')
+wantedWord = input('please enter a word that you are looking for! like Ariana : ')
 
 # looking for the wanted word
-x = re.search("^The.*Spain$", txt)
+x = re.search( wantedWord, f.read())
+print(x)
 
 if x:
   print("YES! We have a match!")
   # Create a new file if it does not exist:
-  f = open("result.txt", "w")
-  f.write(txt)
+  f = open("result.txt", "w") # w will overwrite on existing file
+  f.write('yes we have a match')
   f.close() 
 
 else:
   print("No match")
+  f = open("result.txt", "w") 
+  f.write('Unfortunately, I see no match')
+  f.close() 
 
-
-
-print('please get the result.txt file')
+# print('please get the result.txt file')
 
