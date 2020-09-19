6a00531084715be565a2378d6dbfaccbc24f5088
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-09-19 15:46:14 -0700

    reversed(), reverse()

---
+++ 22-reversed.py
@@ -0,0 +1,10 @@
+numbers = [1,2,3,4,5,6]
+# numbers.reverse()
+print(numbers)
+
+print(list(reversed(numbers))) #[6, 5, 4, 3, 2, 1]
+print(list(reversed('hello'))) #['o', 'l', 'l', 'e', 'h']
+print('hello'[::-1]) #olleh
+
+
+
