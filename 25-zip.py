725ff3e0b36f57c53012423ea2142efffc8bbe37
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-09-20 13:37:20 -0700

    zip()

--- 25-zip.py
+++ 25-zip.py
@@ -2,5 +2,6 @@ numbers_1 = [1, 2, 3, 4]
 numbers_2 = [5, 6, 7, 8]
  
 result = zip(numbers_1, numbers_2)
-print(list(result)) #[(1, 5), (2, 6), (3, 7), (4, 8)]
+# print(list(result)) #[(1, 5), (2, 6), (3, 7), (4, 8)]
 
+print(dict(result)) #{1: 5, 2: 6, 3: 7, 4: 8}
