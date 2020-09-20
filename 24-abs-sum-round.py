ebd05f5477b9e0ad648ee83054ef7be1173ee5d5
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-09-20 13:31:49 -0700

    abs(), sum(), round()

---
+++ 24-abs-sum-round.py
@@ -0,0 +1,11 @@
+# absolute value
+num = -7.5
+print(abs(num)) #7.5
+
+# sum
+numbers = [3, 7] # it can be tuple too
+print(sum(numbers, 10)) #10 +10
+
+# round
+digit = 2.988888
+print(round(digit, ndigits= 2)) #2.99
