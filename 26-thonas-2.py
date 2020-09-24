0a9335eab5cd11294f215f9a06dcb6c2fa52cd48
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-09-23 21:55:45 -0700

    thanos 2

--- 26-thonas-2.py
+++ 26-thonas-2.py
@@ -55,10 +55,51 @@ import re
 # #   for lines in csvFile: 
 # #         print(lines) 
   
-# # search for a word in cvs file
+# # # search for a word in cvs file with panda library
+# import pandas 
+# # reading the CSV file 
+# csvFile = pandas.read_csv('sample_tweets.csv')  
+# # displaying the contents of the CSV file 
+# #print(csvFile) 
 
-import pandas 
-# reading the CSV file 
-csvFile = pandas.read_csv('sample_tweets.csv')  
-# displaying the contents of the CSV file 
-print(csvFile) 
+#####################################################################
+
+username = input('Enter:  ')
+file = open("new.txt", "w")
+with open('sample_tweets.csv', 'rt') as f:
+     reader = csv.reader(f, delimiter=',') # good point by @paco
+     for row in reader:
+          for field in row:
+              if field == username:
+                  print( "is in file")
+                  file.write(row)
+#You Must Close the FIle after writing it.
+                  file.close()
+
+#####################################################################
+# username = 'goli'
+# is_in_file = False
+# with open('sample_tweets.csv', 'rb') as csvfile:
+#     my_content = csv.reader(csvfile, delimiter=',')
+#     for row in my_content:
+#         if username in row:
+#             is_in_file = True
+# print ('is_in_file')
+####################################################################
+# # third approach - successfull, read from a csv file and make a txt file and write on it
+# #Open file with "w"(write), the file will create automatically.
+# file = open("new.txt", "w")
+# #asking user to put favorite word
+# wantedword = input('Enter the word you are Looking for: ')
+# #Open CSV file to read CSV, note: reading and write file should be under "with"
+# with open('sample_tweets.csv') as csvFile:
+#     #Read CSV
+#     readCsv = csv.reader(csvFile)
+#     for row in readCsv:
+#         #Get Values and manupilate in the file.write
+#         Id = row[0]
+#         Id1 = row[1]
+#         #Write CSV you need format it to string if the value is an int
+#         file.write("UPDATE Schema.TableName SET Column1="+str(Id1)+" where Column2="+str(Id)+";\n")
+# #You Must Close the FIle after writing it.
+# file.close()
