290ffd3a26644201030d7513673e4fec836de62c
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-09-24 17:23:27 -0700

    thanos 2

---
+++ 27-thanos-3.py
@@ -0,0 +1,28 @@
+import re
+
+# opening sample_tweets file
+f = open("sample_tweets 2.txt", "r")
+# print(f.read())
+
+# asking user to enter wanted word
+wantedWord = input('please enter a word that you are looking for! like Ariana : ')
+
+# looking for the wanted word
+x = re.search( wantedWord, f.read())
+print(x)
+
+if x:
+  print("YES! We have a match!")
+  # Create a new file if it does not exist:
+  f = open("result.txt", "w") # w will overwrite on existing file
+  f.write('yes we have a match:   ')
+  f.write(str(x))
+  f.close() 
+
+else:
+  print("No match")
+  f = open("result.txt", "w") 
+  f.write('Unfortunately, I see no match')
+  f.close() 
+
+# print('please get the result.txt file')
