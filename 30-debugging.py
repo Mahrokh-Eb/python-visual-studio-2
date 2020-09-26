37d57d259fb575e2b7a174540c965fc01b38b0a7
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-09-26 13:51:33 -0700

    debugging commands

---
+++ 30-debugging.py
@@ -0,0 +1,8 @@
+# pythom debugger
+import pdb
+pdb.set_trace( )
+
+number1 = int(input('Ebter num-1: '))
+number2 = int(input('Ebter num-2: '))
+total = number1 + number2
+print(f'the sume of number1 and number2= {total}') 
