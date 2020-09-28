7fc46d4c90598ca862d18903a38b3605770ede1c
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-09-28 12:19:36 -0700

    module defining

--- 31-module.py
+++ 31-module.py
@@ -1,5 +1,8 @@
-import random
+#import random
+#import random as rand
+from random import randint, choice
+# from random import *
 
-print(random.randint(1, 100))
-myCourses = ['Python', 'xml', 'jquery', 'java', 'kotlin']
-print(random.choice(myCourses ))
+print(randint(1, 100)) # gives a number beetween 1, 100
+myCourses = ['Python', 'xml', 'jquery', 'java', 'kotlin'] 
+print(choice(myCourses )) # select one of the above list
