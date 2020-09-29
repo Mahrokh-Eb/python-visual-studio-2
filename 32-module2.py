9fac81a5ca0eef6b2c2f18f082a292e016e8c90d
Mahrokh-Eb <mebrahimi@ucla.edu>
2020-09-29 11:04:35 -0700

    module 2

---
+++ 32-module2.py
@@ -0,0 +1,8 @@
+# making colors
+import termcolor
+print(termcolor.colored('Python in color', color = 'red'))
+print(termcolor.colored('Python in color', color = 'green'))
+print(termcolor.colored('Python in color', color = 'yellow'))
+print(termcolor.colored('Python in color', color = 'blue'))
+print(termcolor.colored('Python in color', color = 'cyan '))
+
